2e1d130cbab4ca5e5b7ec1ae105ffa5eba8fcdc8
uniaojoaquinense <93949448+uniaojoaquinense@users.noreply.github.com>
2026-03-02 18:57:25 -0300

    volta variables

--- config.js
+++ config.js
@@ -3,9 +3,9 @@
 // Edite APENAS este arquivo para alterar credenciais
 // ═══════════════════════════════════════════════════════
 const CONFIG = {
-    API_KEY: 'AIzaSyDS2qfgwQLIdGJbrThWraROwj7-5yTLvSA',
-    CLIENT_ID: '173684456547-n01u603usrbocdb4mgb5potr5uhkhr1u.apps.googleusercontent.com',
-    SHEET_ID: '1lyyBGpjuNZdvHID3Iqte89DmilsSn8Ovq1zueazMG98',
+    API_KEY: 'AIzaSyAmW-hkAUHCnYc_4CIcN99HiNAlbc31-Qs',
+    CLIENT_ID: '897683631001-ugml9ertq7bldbtmsugcejhitav6l4dp.apps.googleusercontent.com',
+    SHEET_ID: '14wrXo2lohTXepgApiqsSQNIzaNH1bvjMLOaCXK65hWU',
     SCOPES: 'https://www.googleapis.com/auth/spreadsheets profile email',
     SHEET_LINKS: 'Sheet1',
     SHEET_CONFIG: 'Sheet2',
