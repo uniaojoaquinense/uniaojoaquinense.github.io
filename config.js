1baf0008a166acacbc49c43790d8d044a8fffbdc
Miguel <silva.silva@unesp.br>
2026-08-05 16:40:23 -0300

    config: aponta proxy e planilha corretos do uniaojoaquinense

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    PROXY_URL: 'https://demolay-proxy.testemiguel319.workers.dev',
+    PROXY_URL: 'https://demolay-proxy.uniaojoaquinense.workers.dev',
     API_KEY: '',
-    SHEET_ID: '1KC4ESOIz_sm9Kq9wPeDH1fUd4WQpmKbrqVOzxG7LuQY',
+    SHEET_ID: '1hDyxeHgEF3OH6ApBzZveZY_DPdoElLuLGt32lsJYFKg',
     SCOPES: 'https://www.googleapis.com/auth/spreadsheets profile email',
     SHEET_LINKS: 'Sheet1',
     SHEET_CONFIG: 'Sheet2',
